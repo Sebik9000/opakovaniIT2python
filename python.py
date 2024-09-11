a582e8af85d500430cc2a728de5859922f057cc3
Sebik9000 <159123960+Sebik9000@users.noreply.github.com>
2024-09-11 08:19:56 +0000

    commit

---
+++ python.py
@@ -0,0 +1,23 @@
+
+#Úkol 1
+name = "sebastian"
+
+#Úkol 2
+surname = "gargosch"
+
+#Úkol 3
+print (name+surname)
+
+#Úkol 4
+input("Zadejte svůj věk: ")
+
+#Úkol 9¨
+
+answer = input("Vlož svůj věk: ")
+
+while not answer.isdigit():
+    print("Zadej jen celočíselnou hodnotu")
+    break
+while True:
+    print("Děkuji.")
+    break
